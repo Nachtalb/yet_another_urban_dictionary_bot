22ca5e3380a4eaf4f93c3d60f059336251ffbc15
Nick Espig <nickespig@gmail.com>
2017-12-08 16:36:07 +0100

    Fix links to other bots

--- yet_another_dictionary_bot/commands.py
+++ yet_another_dictionary_bot/commands.py
@@ -29,7 +29,7 @@ Please share this bot with your friends so that I ([the magician](https://github
 have enough motivation to continue and maintain this bot. 
  
 Check out my other project\[s\]: 
-- [@insta_looter_bot](https://github.com/Nachtalb/insta_looter_bot) - Download images and videos from Instagram via 
+- [@insta_looter_bot](https://t.me/insta_looter_bot) - Download images and videos from Instagram via 
 Telegram
 - [@reverse_image_search_bot](https://t.me/reverse_image_search_bot) - Reverse image search directly in  
 Telegram
